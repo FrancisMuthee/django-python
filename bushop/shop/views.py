720d72359e300254bb0b34a73dc761fe8b7d5b95
0757377860 <francisnjaramba2@gmail.com>
2024-04-05 14:12:31 +0300

    static files update

--- bushop/shop/views.py
+++ bushop/shop/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'base.html')
+    return render(request, 'index.html', {'name': 'El-Franko'})
