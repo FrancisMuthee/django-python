0e2e121eb669f3c0eaf62bd8d7ef4b1f880ec8d9
0757377860 <francisnjaramba2@gmail.com>
2024-04-13 12:01:44 +0300

    Page redirect

--- shop/urls.py
+++ shop/urls.py
@@ -14,6 +14,10 @@ app_name = 'shop'
 urlpatterns = [
     # Home page
     path('', views.home, name='home'),
+    path('shop/home', views.home, name= "home"),
     path('shop/products', views.products , name='products'),
+    path('shop/testimonials', views.testimonials, name= 'testimonials'),
+    path('shop/contact', views.contact, name='contact')
+
    ]
 
