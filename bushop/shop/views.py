8fc9231f4a29507c087cf9085c4ac45e6d5fd83a
0757377860 <francisnjaramba2@gmail.com>
2024-04-13 14:36:55 +0300

    All small changes

--- bushop/shop/views.py
+++ bushop/shop/views.py
@@ -9,6 +9,7 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import items
 
 # Create your views here.
 def home(request):
@@ -16,7 +17,28 @@ def home(request):
     return render(request, 'base.html')
 
 def products(request):
-    return render(request, 'products.html')
+
+    item1 = items()
+    item1.price = 'Ksh. 700'
+    item1.name = 'Latte'
+    item1.img = 'cup.jpeg'
+    item1.desc = 'I want some latted coffee'
+
+    item2 = items()
+    item2.price = 'Ksh. 150'
+    item2.name = 'Latte'
+    item2.img = 'cnn.jpg'
+    item2.desc = 'As black as...'
+
+    item3 = items()
+    item3.price = 'Ksh. 999'
+    item3.name = 'Latte'
+    item3.img = 'Arabica.jpg'
+    item3.desc = 'From farm to cup'
+
+    itemss = [item1, item2, item3]
+
+    return render(request, 'products.html', {'itemss': itemss})
 
 def testimonials(request):
     return render(request, 'testimonials.html')
